15d0b419a460f5d138925ebf455dd4f6bf555acd
LeoAndres10 <leo1318velasquez@hotmail.es>
2024-08-09 20:49:11 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+
+        public double n1 { get; set; }
+        public double n2 { get; set; }
+
+
+        public double Suma(double n1, double n2)
+        {
+
+            return n1 + n2;
+        }
+
+        public double Resta(double n1, double n2)
+        {
+
+            return n1 - n2;
+        }
+
+        public double multiplicacion(double n1, double n2)
+        {
+
+            return n1 * n2;
+        }
+
+        public double Division(double n1, double n2)
+        {
+
+            return n1 / n2;
+        }
+    }
+}
---
+++ EjercicioExcepciones/Program.cs
@@ -0,0 +1,119 @@
+﻿
+using EjercicioExcepciones;
+using System.Linq.Expressions;
+
+Operaciones operaciones = new Operaciones();
+int opcion = 0;
+double n1, n2;
+
+while (opcion!=5)
+{
+
+    Console.WriteLine("******MENU******");
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicacion");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Salir");
+    opcion=Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            try { 
+            Console.WriteLine("SUMA");
+            Console.WriteLine("Ingrese primer numero: ");
+            n1=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            operaciones.Suma(n1, n2);
+
+            }catch (FormatException e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.ToString()}");
+            }
+            break;
+
+            case 2:
+
+            try { 
+            Console.WriteLine("Resta");
+            Console.WriteLine("Ingrese primer numero: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            operaciones.Resta(n1, n2);
+            }catch (FormatException e)
+            {
+        Console.WriteLine(e.ToString());
+
+            }catch (Exception e)
+            {
+        Console.WriteLine($"Error: {e.ToString()}");
+            }
+
+    break;
+
+            case 3:
+            try
+            {
+                Console.WriteLine("MULTIPLICACION");
+                Console.WriteLine("Ingrese primer numero: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese segundo numero: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+
+                operaciones.multiplicacion(n1, n2);
+            }catch (FormatException e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.ToString()}");
+            }
+
+            break;
+
+
+            case 4:
+
+            try
+            {
+                Console.WriteLine("DIVISION");
+                Console.WriteLine("Ingrese primer numero: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese segundo numero: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+
+                operaciones.Division(n1, n2);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.ToString()}");
+            }
+            break;
+
+            default: Console.WriteLine("Opcion incorrecta");
+
+            break;
+    }
+
+}
